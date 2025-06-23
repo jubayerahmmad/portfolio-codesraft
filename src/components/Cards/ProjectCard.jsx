287afe4409298483523d3b7b205b1958e4d219d2
jubayerahmmad <zubayersami510@gmail.com>
2025-06-23 17:07:24 +0600

    feat: Project section Carausel feature implemented.

--- src/components/Cards/ProjectCard.jsx
+++ src/components/Cards/ProjectCard.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa6";
 const ProjectCard = ({ project }) => {
   const { name, description, image, live, github, tags } = project || {};
   return (
-    <div className="bg-cyan-100/10 rounded-2xl transition duration-300">
+    <div className="flex-none bg-cyan-100/10 rounded-2xl transition duration-300 w-full md:w-1/2">
       <div>
         <img
           src={image}
--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -1,29 +1,70 @@
 import { Link } from "react-router-dom";
-import projects from "../../data/projects";
 import ProjectCard from "../Cards/ProjectCard";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { useState } from "react";
+import projects from "../../data/projects";
 
 const Projects = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  // console.log(currentIndex);
+  const itemPerSlide = 2;
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => {
+      const prev = prevIndex - itemPerSlide;
+      return prev <= 0 ? 0 : prev;
+    });
+  };
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => {
+      const nextIndex = prevIndex + itemPerSlide;
+      return nextIndex;
+    });
+  };
+
   return (
     <section className="w-full py-24 bg-cyan-950/40">
       <div className="max-w-7xl mx-auto px-4">
-        {/* header */}
-        <div className="flex flex-col gap-4 justify-start">
-          <h1 className="section-header">MY RECENT WORKS</h1>
-          <h2 className="text-2xl md:text-4xl font-bold">Featured Projects</h2>
+        <div className="flex items-center justify-between">
+          {/* header */}
+          <div className="flex flex-col gap-4 justify-start">
+            <h1 className="section-header">MY RECENT WORKS</h1>
+            <h2 className="text-2xl md:text-4xl font-bold">
+              Featured Projects
+            </h2>
+          </div>
+          {/* carousel controller */}
+          <div className="flex items-center gap-2">
+            <button
+              onClick={handlePrev}
+              disabled={currentIndex === 0}
+              className="p-3 rounded-full bg-cyan-700 cursor-pointer hover:bg-cyan-800 disabled:bg-cyan-950"
+            >
+              <FaArrowLeft size={24} />
+            </button>
+            <button
+              onClick={handleNext}
+              disabled={currentIndex === projects.length}
+              className="p-3 rounded-full bg-cyan-700 cursor-pointer hover:bg-cyan-800 disabled:bg-cyan-950 disabled:cursor-not-allowed"
+            >
+              <FaArrowRight size={24} />
+            </button>
+          </div>
         </div>
 
         {/* Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 pt-10">
-          {projects.slice(0, 3).map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
+        <div className="overflow-hidden pt-10">
+          <div
+            className="flex gap-6 transition-transform duration-500 ease-in-out"
+            style={{
+              transform: `translateX(-${(currentIndex * 100) / itemPerSlide}%)`,
+            }}
+          >
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className="mt-6 flex justify-center">
-        <Link to="/projects">
-          <button className="btn-primary">View All Projects</button>
-        </Link>
       </div>
     </section>
   );
